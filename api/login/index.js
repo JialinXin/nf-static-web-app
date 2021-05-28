5edadb09037cc8e31267c4f12ac4f24fb67a36ec
Jialin Xin <jixin@microsoft.com>
2021-05-28 11:27:06 +0800

    update

--- api/login/index.js
+++ api/login/index.js
@@ -1,7 +1,18 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//module.exports = async function (context, req) {
+//    context.log('JavaScript HTTP trigger function processed a request.');
+//
+//    const name = (req.query.name || (req.body && req.body.name));
+//    const responseMessage = name
+//        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+//        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+//
+//    context.res = {
+//        // status: 200, /* Defaults to 200 */
+//        body: responseMessage
+//    };
+//}
 
-module.exports = function (context, req, connection) {
-  context.res = { body: connection };
-  context.done();
-};
+module.exports = async function (context, req, connection) {
+    context.res = { body: connection };
+    //context.done();
+  };
